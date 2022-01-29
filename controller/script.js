426e54aca53b24c4d0e7f2cf24fcca62a2c11c5f
Vaibhav Mittal <vaibhav8078672060@gmail.com>
2022-01-29 17:26:44 +0530

    changes made

--- controller/script.js
+++ controller/script.js
@@ -2,7 +2,9 @@ const request =require('request');
 const cheerio = require('cheerio');
 
    let links=[];
-   links.length=0;
+   let title=[];
+   let author=[];
+   let tags=[];
   
     const webid=`https://medium.com/tag/developer/latest`;
     request(webid,function(err,response,html){
@@ -26,6 +28,36 @@ const cheerio = require('cheerio');
                 }
             }
 
+            var Ael = $('h2');
+            for(var i=0; i<Ael.length; i++)
+            {
+                var el = Ael.eq(i);
+            
+                if(i<10)
+                 title.push($(el).text());
+            }
+
+            var Ael = $('h4');
+            for(var i=0; i<Ael.length; i++)
+            {
+                var el = Ael.eq(i);
+            
+                if($(el).attr().class!=undefined && $(el).attr().class.length==38)
+                   author.push($(el).text());
+            }
+
+            var Ael = $('a');
+            for(var i=0; i<Ael.length; i++)
+            {
+                var el = Ael.eq(i);
+            
+                if($(el).attr().class!=undefined && $(el).attr().class.length==2 && $(el).children().length==1 && tags.includes($(el).text())==false)
+                  {tags.push($(el).text());}
+            }
+
             console.log(links);
+            console.log(title);
+            console.log(author);
+            console.log(tags);
         }
     });
