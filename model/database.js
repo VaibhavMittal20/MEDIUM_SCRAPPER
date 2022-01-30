7331a2019795a6482d868c9c5c95b5c6e6913610
Vaibhav Mittal <vaibhav8078672060@gmail.com>
2022-01-30 22:34:00 +0530

    changes done to database.js

--- model/database.js
+++ model/database.js
@@ -8,13 +8,51 @@ var conn = mysql.createConnection({
     port: 3306
 });
 
-
-conn.connect(function(error){
-    if(!!error){
+conn.connect((err)=>{
+    if(!!err){
         console.log('Error');
     }
     else{
         console.log('Connected');
     }
 });
+
+let tag=[];
+let date=[];
+
+function processquery()
+{
+    tag.length=0;
+    date.length=0;
+
+    conn.query("select tag from info order by id desc",(err,data)=>{
+        if(!!err){
+            console.log('Error in the Query');
+        }
+        else {
+            console.log('Successful Query');
+            
+            for(let i=0;i<data.length;i++)
+            {
+                tag.push(data[i].tag);
+            }
+         
+        }
+    });
+
+    conn.query("select date from info order by id desc",(err,data)=>{
+        if(!!err){
+            console.log('Error in the Query');
+        }
+        else {
+            console.log('Successful Query');
+            
+            for(let i=0;i<data.length;i++)
+            {
+                date.push(data[i].date);
+            }
+           
+        }
+    });
+}
 
